fb619ce51653d0da814e6c8ecdd7a89cd209402b
Laura Beth Maddox <laurabeth.maddox@msimga.com>
2023-07-17 10:07:28 -0500

    Added series repository

---
+++ src/ComicBookGallery/ComicBookGallery/Data/SeriesRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int id)
+        {
+            Series seriesToReturn = null;
+
+            foreach (var series in Data.Series)
+            {
+                if (series.Id == id)
+                {
+                    seriesToReturn = series;
+                    break;
+                }
+            }
+
+            if (seriesToReturn != null)
+            {
+                // TODO Get the comic books for this series
+                var comicBooks = new ComicBook[0];
+
+                foreach (var comicBook in comicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.Id == id)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+                }
+
+                seriesToReturn.Issues = comicBooks;
+            }
+
+            return seriesToReturn;
+        }
+    }
+}
