c0039690a8dc41bbb9bd2601034715c6cf67b67d
Laura Beth Maddox <laurabeth.maddox@msimga.com>
2023-07-17 09:44:19 -0500

    Added Data class

--- src/ComicBookGallery/ComicBookGallery/Models/Series.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/Series.cs
@@ -9,7 +9,7 @@ namespace ComicBookGallery.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string DescriptionHtml { get; set; }
         public ComicBook[] Issues { get; set; }
     }
 }
